01b6e31978e98b2c3d3834b1e6d42ac23d402822
Rafael <email@example.comluizcarlospimentelribeiro@gmail.comgit config --global user.email email@example.comluizcarlospimentelribeiro@gmail.com>
2021-10-31 15:18:45 -0300

    mudando URL da API para funcionar no netlify

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const nome = document.getElementById('name');
  
-fetch(`http://ergast.com/api/f1/2021/driverStandings.json`).then(resposta => {
+fetch(`https://ergast.com/api/f1/2021/driverStandings.json`).then(resposta => {
         return resposta.json();
         }).then(data => {
                 const datas = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
